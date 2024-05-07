016b43152a5fd7a9eb1cada8a1d26beddf84134c
ivbob1 <ivan.p.bobrik@gmail.com>
2024-05-07 13:43:01 +0300

    update

--- static/drawing.js
+++ static/drawing.js
@@ -1,19 +1,27 @@
 
 let canvas = document.getElementById('canvas');
-canvas.width = window.innerWidth * 0.9;
-canvas.height = window.innerHeight * 0.9;
+// canvas.width = window.innerWidth * 0.9;
+// canvas.height = window.innerHeight * 0.9;
 
-canvas.style.width = `${window.innerWidth * 0.9}px`;
-canvas.style.height = `${window.innerHeight * 0.9}px`;
-
+// canvas.style.width = `${window.innerWidth * 0.9}px`;
+// canvas.style.height = `${window.innerHeight * 0.9}px`;
 let context = canvas.getContext('2d');
 let width = canvas.width;
 let height = canvas.height;
-
-currentlineWidth = 4;
-context.globalAlpha = 1.0;
-context.imageSmoothingEnabled = true;
-context.lineCap = 'round';
+let currentlineWidth = 4;
+function updateCanvasSize() {
+    canvas.width = canvas.parentElement.offsetWidth;
+    canvas.height = canvas.parentElement.offsetHeight;
+    width = canvas.width;
+    height = canvas.height;
+    sc_topLeft={x: 0, y:height};
+    sc_bottomRight={x: width, y: 0};
+    context = canvas.getContext('2d');
+    currentlineWidth = 4;
+    context.globalAlpha = 1.0;
+    context.imageSmoothingEnabled = true;
+    context.lineCap = 'round';
+}
 
 let drawing = false;
 let lastPosition = { x: 0, y: 0 };
@@ -55,10 +63,20 @@ function convert_to_cord(point){
     y+=sc_topLeft.y
     return { x: x, y: y }; 
 }
-window.onload = function() {
+function redraw(){
     for(let i=0; i < points.length; i++){
         drawLine( points[i].from, points[i].to,points[i].lineWidth/zoom_scale);
     }
+}
+window.onload = function() {
+    updateCanvasSize();
+    redraw();
+};
+window.onresize = function() {
+    updateCanvasSize();
+    console.log("ok");
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    redraw();
 };
 
 canvas.onmousedown = function(e) {
@@ -72,36 +90,37 @@ canvas.onmouseup = function() {
 let currentPosition;
 canvas.onmousemove = function(e) {
     currentPosition = getMousePosition(canvas, e);
-    if(drawing) {
-        if((currentPosition.x-lastPosition.x)**2+(currentPosition.y-lastPosition.y)**2>currentlineWidth){
-            socket.emit('draw', { from: convert_to_cord(lastPosition), to: convert_to_cord(currentPosition) , lineWidth: (currentlineWidth*zoom_scale) });
-            lastPosition = currentPosition;
-        }
-    }
 };
 let zoom_scale=1;
 let zoom_scale_coef=0.1; 
 let zoom_count=0;
+function zoomIn(position){
+    sc_topLeft.x += (position.x-sc_topLeft.x)*zoom_scale_coef;
+    sc_topLeft.y -=(sc_topLeft.y-position.y)*zoom_scale_coef;
+    sc_bottomRight.x += (position.x-sc_bottomRight.x)*zoom_scale_coef;
+    sc_bottomRight.y -= (sc_bottomRight.y-position.y)*zoom_scale_coef;
+    zoom_count++;
+    zoom_scale=(1-zoom_scale_coef)**zoom_count
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    redraw();
+}
+function zoomOut(position){
+    sc_topLeft.x -= (position.x-sc_topLeft.x)*(zoom_scale_coef/(1-zoom_scale_coef));
+    sc_topLeft.y +=(sc_topLeft.y-position.y)*(zoom_scale_coef/(1-zoom_scale_coef));
+    sc_bottomRight.x -= (position.x-sc_bottomRight.x)*(zoom_scale_coef/(1-zoom_scale_coef));
+    sc_bottomRight.y += (sc_bottomRight.y-position.y)*(zoom_scale_coef/(1-zoom_scale_coef));
+    zoom_count--;
+    zoom_scale=(1-zoom_scale_coef)**zoom_count
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    redraw();
+}
 canvas.addEventListener('wheel', function(event) {
     let position = convert_to_cord(currentPosition);
     if (event.deltaY < 0) {
-        sc_topLeft.x += (position.x-sc_topLeft.x)*zoom_scale_coef;
-        sc_topLeft.y -=(sc_topLeft.y-position.y)*zoom_scale_coef;
-        sc_bottomRight.x += (position.x-sc_bottomRight.x)*zoom_scale_coef;
-        sc_bottomRight.y -= (sc_bottomRight.y-position.y)*zoom_scale_coef;
-        zoom_count++;
+        zoomIn(position);
     } else if (event.deltaY > 0) {
-        sc_topLeft.x -= (position.x-sc_topLeft.x)*(zoom_scale_coef/(1-zoom_scale_coef));
-        sc_topLeft.y +=(sc_topLeft.y-position.y)*(zoom_scale_coef/(1-zoom_scale_coef));
-        sc_bottomRight.x -= (position.x-sc_bottomRight.x)*(zoom_scale_coef/(1-zoom_scale_coef));
-        sc_bottomRight.y += (sc_bottomRight.y-position.y)*(zoom_scale_coef/(1-zoom_scale_coef));
-        zoom_count--;
+        zoomOut(position);
     }
-    zoom_scale=(1-zoom_scale_coef)**zoom_count
-    console.log(zoom_scale);
-    console.log((sc_bottomRight.x-sc_topLeft.x));
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    window.onload();
     // Предотвращаем прокрутку страницы
     event.preventDefault();
 }, { passive: false });
@@ -109,9 +128,11 @@ canvas.addEventListener('wheel', function(event) {
 
 function getMousePosition(canvas, event) {
     let rect = canvas.getBoundingClientRect();
+    let scaleX = canvas.width / rect.width;    // отношение реального размера к размеру CSS
+    let scaleY = canvas.height / rect.height;  // отношение реального размера к размеру CSS
     return {
-        x: event.clientX - rect.left,
-        y: event.clientY - rect.top
+        x: (event.clientX - rect.left) * scaleX,   // масштабирование координат мыши после учета прокрутки страницы
+        y: (event.clientY - rect.top) * scaleY
     };
 }
 
@@ -125,12 +146,113 @@ function drawLine(from, to, lineWidth) {
         context.lineTo(to.x, to.y);
         context.stroke();
     }
-}   
+}
+document.getElementById('zoomIn_button').addEventListener('click', function() {
+    zoomIn({x:width/2,y:height/2});
+});
+
+document.getElementById('zoomOut_button').addEventListener('click', function() {
+    zoomOut({x:width/2,y:height/2});
+});
+let tools = {
+    brush: {
+        mousedown: function(e) {
+            canvas.addEventListener('mousemove', tools.brush.mousemove);
+            lastPosition = getMousePosition(canvas, e);
+        },
+        mousemove: function(e) {
+            currentPosition = getMousePosition(canvas, e);
+            if((currentPosition.x-lastPosition.x)**2+(currentPosition.y-lastPosition.y)**2>currentlineWidth){
+                socket.emit('draw', { from: convert_to_cord(lastPosition), to: convert_to_cord(currentPosition) , lineWidth: (currentlineWidth) });
+                lastPosition = currentPosition;
+            }
+        },
+        mouseup: function() {
+            canvas.removeEventListener('mousemove', tools.brush.mousemove);
+        }
+    },
+    eraser: {
+        mousedown: function(e) {
+            canvas.addEventListener('mousemove', tools.eraser.mousemove);
+            lastPosition = getMousePosition(canvas, e);
+        },
+        mousemove: function(e) {
+            currentPosition = getMousePosition(canvas, e);
+            let Top
+            socket.emit('erase', { topLeft: topLeft, bottomRight: bottomRight});
+            lastPosition = currentPosition;
+        },
+        mouseup: function() {
+            canvas.removeEventListener('mousemove', tools.brush.mousemove);
+        }
+    },
+    // Добавьте здесь другие инструменты...
+};
+let activeTool = null;
+function selectTool(tool) {
+    if (activeTool) {
+        activeTool.classList.remove('active');
+        // Удалить старые обработчики событий
+        if(tools.hasOwnProperty(activeTool.id)){
+            canvas.removeEventListener('mousedown', tools[activeTool.id].mousedown);
+            canvas.removeEventListener('mouseup', tools[activeTool.id].mouseup);
+        }
+    }
+    tool.classList.add('active');
+    activeTool = tool;
+    // Добавить новые обработчики событий
+    console.log(tool.id);
+    if(tools.hasOwnProperty(tool.id)){
+        canvas.addEventListener('mousedown', tools[tool.id].mousedown);
+        canvas.addEventListener('mouseup', tools[tool.id].mouseup);
+    }
+}
+
+document.getElementById('brush').addEventListener('click', function() {
+    selectTool(this);
+
+    // Код для выбора кисти
+});
+
+document.getElementById('eraser').addEventListener('click', function() {
+    selectTool(this);
+    // Код для выбора ластика
+});
+
+document.getElementById('move').addEventListener('click', function() {
+    selectTool(this);
+    // Код для выбора инструмента перемещения
+});
+let modal = document.getElementById("brushModal");
+let brushBtn = document.getElementById("brush");
+let span = document.getElementsByClassName("close")[0];
+
+// Открываем модальное окно только при нажатии на кнопку "Кисть"
+brushBtn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// Закрываем модальное окно при нажатии на "x"
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// Закрываем модальное окно, если пользователь кликнул вне его
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 let socket = io.connect('http://' + document.domain + ':' + location.port + '/draw');
 socket.on('connect', function() {
     socket.emit('join1');
 });
 socket.on('draw', function(data) {
+    points.push(data);
+    console.log(data.lineWidth)
+    drawLine(data.from,data.to, data.lineWidth/zoom_scale);
+});
+socket.on('erase', function(data) {
     points.push(data);
     console.log(data.lineWidth)
     drawLine(data.from,data.to, data.lineWidth/zoom_scale);
